697b47b297d51efff47dadfbc4273ddf1a459baa
hannahvergio <vergio.hannahgrace@gmail.com>
2025-03-24 13:12:55 +0800

    Initial commit on quiz branch

---
+++ hannah/project/proj/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Car, RentalLocation, Rental, Review, Point
+
+admin.site.register(Car)
+admin.site.register(RentalLocation)
+admin.site.register(Rental)
+admin.site.register(Review)
+admin.site.register(Point)
+
